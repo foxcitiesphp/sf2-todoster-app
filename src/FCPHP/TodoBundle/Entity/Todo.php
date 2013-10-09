5bc5bc9a475a6c53c696a9fe5746f0d2480ad3ef
Mathew Peterson <cpress@gmail.com>
2013-10-09 16:11:19 -0500

    Adds Todo entity and repository.

---
+++ src/FCPHP/TodoBundle/Entity/Todo.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace FCPHP\TodoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Todo
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="FCPHP\TodoBundle\Entity\TodoRepository")
+ */
+class Todo
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="completed", type="boolean")
+     */
+    private $completed;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Todo
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set completed
+     *
+     * @param boolean $completed
+     * @return Todo
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+    
+        return $this;
+    }
+
+    /**
+     * Get completed
+     *
+     * @return boolean 
+     */
+    public function getCompleted()
+    {
+        return $this->completed;
+    }
+}
