3bfb5a759fa0961f4d307aa459aff490ce8dc368
Mathew Peterson <cpress@gmail.com>
2013-10-09 17:31:21 -0500

    Adds ability to mark todos as complete.

--- src/FCPHP/TodoBundle/Controller/DefaultController.php
+++ src/FCPHP/TodoBundle/Controller/DefaultController.php
@@ -3,12 +3,13 @@
 namespace FCPHP\TodoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-		/** @var \PCPHP\TodoBundle\Entity\TodoRepository $repo */
+		/** @var \FCPHP\TodoBundle\Entity\TodoRepository $repo */
 		$repo = $this->container->get('doctrine')->getRepository('TodoBundle:Todo');
 
 		$incomplete = $repo->findIncompleteTodos();
@@ -19,4 +20,26 @@ class DefaultController extends Controller
 			'completed' => $completed
 		));
     }
+
+	public function completeAction($id)
+	{
+		/** @var \FCPHP\TodoBundle\Entity\TodoRepository $repo */
+		$repo = $this->container->get('doctrine')->getRepository('TodoBundle:Todo');
+
+		/** @var \FCPHP\TodoBundle\Entity\Todo $todo */
+		$todo = $repo->find($id);
+
+		if(!$todo)
+		{
+			throw new NotFoundHttpException('That todo was not found.');
+		}
+
+		$todo->setCompleted(true);
+		/** @var \Doctrine\ORM\EntityManager $em */
+		$em = $this->container->get('doctrine')->getEntityManager();
+		$em->persist($todo);
+		$em->flush();
+
+		return $this->redirect($this->generateUrl('todo_homepage'));
+	}
 }
