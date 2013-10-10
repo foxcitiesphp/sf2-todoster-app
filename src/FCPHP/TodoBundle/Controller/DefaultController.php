aeefb529a47b3e0a8839e25737b36f30f1e5242d
Mathew Peterson <cpress@gmail.com>
2013-10-09 20:11:02 -0500

    Adds updating ability.

--- src/FCPHP/TodoBundle/Controller/DefaultController.php
+++ src/FCPHP/TodoBundle/Controller/DefaultController.php
@@ -2,22 +2,45 @@
 
 namespace FCPHP\TodoBundle\Controller;
 
+use FCPHP\TodoBundle\Entity\Todo;
+use FCPHP\TodoBundle\Form\TodoType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
 		/** @var \FCPHP\TodoBundle\Entity\TodoRepository $repo */
 		$repo = $this->container->get('doctrine')->getRepository('TodoBundle:Todo');
 
+		$todo_form = $this->container->get('form.factory')->create(new TodoType());
+
+		if('POST' === $request->getMethod())
+		{
+			$todo_form->bind($request);
+
+			if($todo_form->isValid())
+			{
+				/** @var \Doctrine\ORM\EntityManager $em */
+				$em = $this->container->get('doctrine')->getEntityManager();
+
+				/** @var \FCPHP\TodoBundle\Entity\Todo $todo */
+				$todo = $todo_form->getData();
+
+				$em->persist($todo);
+				$em->flush();
+			}
+		}
+
 		$incomplete = $repo->findIncompleteTodos();
 		$completed = $repo->findCompletedTodos();
 
-        return $this->render('TodoBundle:Default:index.html.twig', array(
+		return $this->render('TodoBundle:Default:index.html.twig', array(
 			'incomplete' => $incomplete,
-			'completed' => $completed
+			'completed' => $completed,
+			'todo_form' => $todo_form->createView()
 		));
     }
 
